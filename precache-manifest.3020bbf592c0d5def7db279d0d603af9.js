1adfdddc0fafcf70c984305ad8a436227ec9b69f
shyju <shyju.vishu@gmail.com>
2020-09-23 22:05:50 +0530

    Updates

---
+++ precache-manifest.3020bbf592c0d5def7db279d0d603af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bff5a0cc0fd98ac7cfff17ee841198",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "45305d27d081f2c4d7f2",
+    "url": "/covid-19-stats/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "09560f7a4c48af5c901b",
+    "url": "/covid-19-stats/static/css/main.b967441f.chunk.css"
+  },
+  {
+    "revision": "45305d27d081f2c4d7f2",
+    "url": "/covid-19-stats/static/js/2.25232de2.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-19-stats/static/js/2.25232de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09560f7a4c48af5c901b",
+    "url": "/covid-19-stats/static/js/main.487be9ba.chunk.js"
+  },
+  {
+    "revision": "19f278041ef95364c108",
+    "url": "/covid-19-stats/static/js/runtime-main.978602b7.js"
+  }
+]);
